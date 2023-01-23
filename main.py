773c50d3290425bb534c4702fe1d4e3e166570e0
Gal Eytan <galeytan1@gmail.com>
2023-01-24 00:56:45 +0200

    Finished demands, needs testing

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import string
+import ex5
+import os
+from ex5 import CaesarCipher, VigenereCipher
 
 
 if __name__ == '__main__':
-    text = ['s', 'd', 'f']
-    print(text)
+    # caesar_cipher = CaesarCipher(3)
+    # text = "Mtm is the BEST!"
+    # encrypted_text = caesar_cipher.encrypt(text)
+    # decrypted_text = caesar_cipher.decrypt(encrypted_text)
+    # print(text, encrypted_text, decrypted_text)
+    # vigenere_cipher = VigenereCipher([7, 8, 11, 13, -2, 4])
+    # text = "come to Rivendell!"
+    # encrypted_text = vigenere_cipher.encrypt(text)
+    # decrypted_text = vigenere_cipher.decrypt(encrypted_text)
+    # print(text, encrypted_text, decrypted_text)
+
+    # for file in os.listdir(path):
+    #     file_path = os.path.join(path, file)
+    #     if os.path.isfile(file_path):
+    #         with open(file_path, 'r’) as f:
+    #         print(f.read())
+    #
+    # Check valid(or exist) Config??? Exceptions???
+    # Check if cipher valid in helper???
+    path = "C:\\Users\\Gal\\Documents\\C C++\\ex5_test"
+    ex5.processDirectory(path)
+
